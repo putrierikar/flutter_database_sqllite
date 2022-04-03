4c327fc5a11b24fc8d5d71a1a5b3b3160e48fc96
putrierikar <putrierikar902@gmail.com>
2022-04-03 23:32:49 +0700

    database sqllite

--- lib/models/entryform.dart
+++ lib/models/entryform.dart
@@ -8,116 +8,159 @@ class EntryForm extends StatefulWidget {
   EntryFormState createState() => EntryFormState(this.item);
 }
 
-
+//class controller
 class EntryFormState extends State<EntryForm> {
-  Item item;
+  Item? item;
   EntryFormState(this.item);
   TextEditingController nameController = TextEditingController();
   TextEditingController priceController = TextEditingController();
-  
+  TextEditingController stokController = TextEditingController();
+  TextEditingController kodeProdukController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     //kondisi
     if (item != null) {
-      nameController.text = item.name;
-      priceController.text = item.price.toString();
+      nameController.text = item!.name;
+      priceController.text = item!.price.toString();
+      stokController.text = item!.stok.toString();
+      kodeProdukController.text = item!.kodeProduk;
     }
     //rubah
     return Scaffold(
-      appBar: AppBar(
-        title: item == null ? Text('Tambah') : Text('Ubah'),
-        leading: Icon(Icons.keyboard_arrow_left),
-      ),
-      body: Padding(
-        padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
-        child: ListView(
-          children: <Widget>[
-            // nama
-            Padding(
-              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-              child: TextField(
-                controller: nameController,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: 'Nama Barang',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                ),
-                onChanged: (value) {
-                  //
-                },
-              ),
-            ),
-            // harga
-            Padding(
-              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-              child: TextField(
-                controller: priceController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Harga',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                ),
-                onChanged: (value) {
-                  //
-                },
-              ),
-            ),
-            // tombol button
-            Padding(
-              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-              child: Row(
-                children: <Widget>[
-                  // tombol simpan
-                  Expanded(
-                    child: RaisedButton(
-                      color: Theme.of(context).primaryColorDark,
-                      textColor: Theme.of(context).primaryColorLight,
-                      child: Text(
-                        'Save',
-                        textScaleFactor: 1.5,
-                      ),
-                      onPressed: () {
-                        if (item == null) {
-                          // tambah data
-                          item = Item(nameController.text,
-                              int.parse(priceController.text));
-                        } else {
-                          // ubah data
-                          item.name = nameController.text;
-                          item.price = int.parse(priceController.text);
-                        }
-                        // kembali ke layar sebelumnya dengan membawa objek item
-                        Navigator.pop(context, item);
-                      },
-                    ),
-                  ),
-                  Container(
-                    width: 5.0,
-                  ),
-                  // tombol batal
-                  Expanded(
-                    child: RaisedButton(
-                      color: Theme.of(context).primaryColorDark,
-                      textColor: Theme.of(context).primaryColorLight,
-                      child: Text(
-                        'Cancel',
-                        textScaleFactor: 1.5,
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: item == null ? Text('Tambah') : Text('Ubah'),
+          leading: Icon(Icons.keyboard_arrow_left),
         ),
-      ),
-    );
+        body: Padding(
+          padding: EdgeInsets.only(top: 20.0, left: 15.0, right: 15.0),
+          child: ListView(
+            children: <Widget>[
+              // nama
+              Padding(
+                padding: EdgeInsets.only(top: 25.0, bottom: 25.0),
+                child: TextField(
+                  controller: nameController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: 'Nama Barang',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    //
+                  },
+                ),
+              ),
+              // harga
+              Padding(
+                padding: EdgeInsets.only(top: 25.0, bottom: 25.0),
+                child: TextField(
+                  controller: priceController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Harga',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    //
+                  },
+                ),
+              ),
+              // stock
+              Padding(
+                padding: EdgeInsets.only(top: 25.0, bottom: 25.0),
+                child: TextField(
+                  controller: stokController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Stok',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    //
+                  },
+                ),
+              ),
+              // kode barang
+              Padding(
+                padding: EdgeInsets.only(top: 25.0, bottom: 25.0),
+                child: TextField(
+                  controller: kodeProdukController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Kode Produk',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    //
+                  },
+                ),
+              ),
+              // tombol button
+              Padding(
+                padding: EdgeInsets.only(top: 25.0, bottom: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    // tombol simpan
+                    Expanded(
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColorDark,
+                        textColor: Theme.of(context).primaryColorLight,
+                        child: Text(
+                          'Save',
+                          textScaleFactor: 1.5,
+                        ),
+                        onPressed: () {
+                          if (item == null) {
+                            // tambah data
+                            item = Item(
+                              nameController.text,
+                              int.parse(priceController.text),
+                              int.parse(stokController.text),
+                              kodeProdukController.text,
+                            );
+                          } else {
+                            // ubah data
+                            item?.name = nameController.text;
+                            item?.price = int.parse(priceController.text);
+                            item?.stok = int.parse(stokController.text);
+                            item?.kodeProduk = kodeProdukController.text;
+                          }
+                          // kembali ke layar sebelumnya dengan membawa objek item
+                          Navigator.pop(context, item);
+                        },
+                      ),
+                    ),
+                    Container(
+                      width: 5.0,
+                    ),
+                    // tombol batal
+                    Expanded(
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColorDark,
+                        textColor: Theme.of(context).primaryColorLight,
+                        child: Text(
+                          'Cancel',
+                          textScaleFactor: 1.5,
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/models/item.dart
+++ lib/models/item.dart
@@ -2,28 +2,43 @@ class Item {
   int? _id;
   String? _name;
   int? _price;
+  int? _stok;
+  String? _kodeproduk;
 
-  int? get id => _id;
-  get price => this._price;
-  get name => this._name;
+  int get id => _id!;
 
-  set id(int? value) => this.id = value;
-  set name(value) => this._name = value;
-  set price(value) => this._price = value;
+  String get name => _name!;
+  set name(String value) => _name = value;
 
-  Item(this._id, this._name, this._price);
+  int get price => _price!;
+  set price(int value) => _price = value;
 
+  int get stok => _stok!;
+  set stok(int value) => _stok = value;
+
+  String get kodeProduk => _kodeproduk!;
+  set kodeProduk(String value) => _kodeproduk = value;
+
+// konstruktor versi 1
+  Item(this._name, this._price, this._stok, this._kodeproduk);
+
+// konstruktor versi 2: konversi dari Map ke Item
   Item.fromMap(Map<String, dynamic> map) {
     this._id = map['id'];
     this._name = map['name'];
     this._price = map['price'];
+    this._stok = map['stok'];
+    this._kodeproduk = map['kodeProduk'];
   }
 
+  // konversi dari Item ke Map
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = Map<String, dynamic>();
     map['id'] = this._id;
     map['name'] = name;
     map['price'] = price;
+    map['stok'] = stok;
+    map['kodeProduk'] = kodeProduk;
     return map;
   }
 }
